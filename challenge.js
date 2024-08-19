f91402d92299110f25de0e5277072d4ae084dbc3
Ibrahim1022 <ibrahimhusseinjama1995@gmail.com>
2024-08-19 20:06:03 +0300

    Done exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+
+let bill = 275; // Assign the bill value here
+let tip;
+
+if (bill >= 50 && bill <= 300) {
+    tip = bill * 0.15; // Calculate 15% tip
+} else {
+    tip = bill * 0.20; // Calculate 20% tip
+}
+
+let total = bill + tip; // Calculate the total value
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
